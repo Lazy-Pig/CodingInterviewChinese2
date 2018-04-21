321d8918fd3a74e1588341ce4ebb139c0540b100
Lazy-Pig <252493575@qq.com>
2018-04-21 16:07:31 +0800

    删除链表中重复的节点

--- 18_删除链表的节点.py
+++ 18_删除链表的节点.py
@@ -6,7 +6,7 @@ class Node(object):
 
 def delete_node(head_p, delete_p):
     """
-    O(1)
+    题目一：O(1)时间内删除链表节点
     """
     if head_p is None or delete_p is None:
         return
@@ -31,13 +31,51 @@ def delete_node(head_p, delete_p):
     return head_p
 
 
+def delete_duplicated_node(head):
+    """
+    题目二：删除链表中重复的节点
+    """
+    if head is None or head.next is None:
+        return head
+
+    new_head = Node(-1)
+    new_p = new_head
+    old_p = head
+
+    while old_p and old_p.next:
+        if old_p.value == old_p.next.value:
+            delete_value = old_p.value
+            while old_p and old_p.value == delete_value:
+                old_p = old_p.next
+            new_p.next = old_p
+        else:
+            new_p.next = old_p
+            new_p = new_p.next
+            old_p = old_p.next
+    return new_head.next
+
+
 if __name__ == "__main__":
-    l1_5 = Node(10)
-    l1_4 = Node(7, l1_5)
-    l1_3 = Node(5, l1_4)
+    # l1_5 = Node(10)
+    # l1_4 = Node(7, l1_5)
+    # l1_3 = Node(5, l1_4)
+    # l1_2 = Node(2, l1_3)
+    # l1_1 = Node(1, l1_2)
+    # head_p = delete_node(l1_1, l1_2)
+    #
+    # while head_p is not None:
+    #     print(head_p.value)
+    #     head_p = head_p.next
+
+    l1_7 = Node(5)
+    l1_6 = Node(4, l1_7)
+    l1_5 = Node(4, l1_6)
+    l1_4 = Node(3, l1_5)
+    l1_3 = Node(3, l1_4)
     l1_2 = Node(2, l1_3)
     l1_1 = Node(1, l1_2)
-    head_p = delete_node(l1_1, l1_2)
+    head = l1_1
+    head_p = delete_duplicated_node(head)
 
     while head_p is not None:
         print(head_p.value)
